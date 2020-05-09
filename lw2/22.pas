5adbcf97f51b5a10e0f2d2f8bac7c890fb64393e
superproger228 <61800512+superproger228@users.noreply.github.com>
2020-05-09 13:23:18 +0300

    Update 22.pas

--- lw2/22.pas
+++ lw2/22.pas
@@ -6,13 +6,11 @@ BEGIN
   WRITELN('Content-Type: text/plain');
   WRITELN;
   Lanterns := GetEnv('QUERY_STRING');
-  IF
-    Lanterns = 'lanterns=1'
+  IF Lanterns = 'lanterns=1'
   THEN
     WRITELN('The British are coming by land')
   ELSE
-    IF
-      Lanterns = 'lanterns=2'
+    IF Lanterns = 'lanterns=2'
     THEN
       WRITELN('The British are coming by sea')
     ELSE
