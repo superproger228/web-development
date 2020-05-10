df4ceedb42726f6ebcbc1e8d4eb381ce22029c88
superproger228 <61800512+superproger228@users.noreply.github.com>
2020-05-10 12:15:06 +0300

    Update 23.pas

--- lw2/23.pas
+++ lw2/23.pas
@@ -10,9 +10,9 @@ BEGIN
     THEN
     IF POS('&', Input) > 0
       THEN
-        WRITELN('Hello dear ', COPY(Input, 6, POS('&', Input) - 6))
+        WRITELN('Hello dear, ', COPY(Input, 6, POS('&', Input) - 6))
       ELSE
-        WRITELN('Hello dear ', COPY(Input, 6, LENGTH(Input) - 4))
+        WRITELN('Hello dear, ', COPY(Input, 6, LENGTH(Input) - 4))
     ELSE
-      WRITELN('Hello anonymous')
+      WRITELN('Hello anonymous.')
 END.
